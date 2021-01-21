e6d2690a980090f215dc57d0a6101e9ffdcbe740
Eless54 <32436964+Eless54@users.noreply.github.com>
2021-01-21 14:25:58 +0100

    Initial commit

---
+++ AyugraAPICSharp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AyugraAPICSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ayugra_list = new System.Windows.Forms.ListBox();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.refresh_button = new System.Windows.Forms.Button();
+            this.disconnect_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ayugra_list
+            // 
+            this.ayugra_list.FormattingEnabled = true;
+            this.ayugra_list.Location = new System.Drawing.Point(12, 22);
+            this.ayugra_list.Name = "ayugra_list";
+            this.ayugra_list.Size = new System.Drawing.Size(155, 95);
+            this.ayugra_list.TabIndex = 0;
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(51, 135);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(75, 23);
+            this.connect_button.TabIndex = 1;
+            this.connect_button.Text = "Connexion";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // refresh_button
+            // 
+            this.refresh_button.Location = new System.Drawing.Point(51, 205);
+            this.refresh_button.Name = "refresh_button";
+            this.refresh_button.Size = new System.Drawing.Size(75, 23);
+            this.refresh_button.TabIndex = 2;
+            this.refresh_button.Text = "Actualiser";
+            this.refresh_button.UseVisualStyleBackColor = true;
+            this.refresh_button.Click += new System.EventHandler(this.refresh_button_Click);
+            // 
+            // disconnect_button
+            // 
+            this.disconnect_button.Location = new System.Drawing.Point(48, 170);
+            this.disconnect_button.Name = "disconnect_button";
+            this.disconnect_button.Size = new System.Drawing.Size(83, 23);
+            this.disconnect_button.TabIndex = 3;
+            this.disconnect_button.Text = "Deconnexion";
+            this.disconnect_button.UseVisualStyleBackColor = true;
+            this.disconnect_button.Click += new System.EventHandler(this.disconnect_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 239);
+            this.Controls.Add(this.disconnect_button);
+            this.Controls.Add(this.refresh_button);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.ayugra_list);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ayugra_list;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.Button refresh_button;
+        private System.Windows.Forms.Button disconnect_button;
+    }
+}
+
---
+++ AyugraAPICSharp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Pipes;
+
+namespace AyugraAPICSharp
+{
+    public partial class Form1 : Form
+    {
+        NamedPipeClientStream client;
+
+        private String[] getAyugraId()
+        {
+            String[] listOfPipes = System.IO.Directory.GetFiles(@"\\.\pipe\");
+            List<String> listOfIds_List = new List<String>();
+            for (int i = 0; i < listOfPipes.Length; i++)
+            {
+                
+                if (listOfPipes[i].Contains("AyugraPacket"))
+                {
+                    Console.WriteLine(listOfPipes[i]);
+                    String[] split = listOfPipes[i].Split('_');
+                    Console.WriteLine(split[1]);
+                    listOfIds_List.Add(split[1]);
+                }
+            }
+
+            String[] listOfIds = listOfIds_List.ToArray();
+            return listOfIds;
+        }
+
+        private NamedPipeClientStream connection(string id)
+        {
+            Console.WriteLine("Connexion..");
+            NamedPipeClientStream client = new NamedPipeClientStream("AyugraPacketApi_" + id);
+            client.Connect();
+            return client;
+        }
+
+        private void refreshList()
+        {
+            String[] ayugraIds = getAyugraId();
+
+            for (int i = 0; i < ayugraIds.Length; i++)
+            {
+                ayugra_list.Items.Add(ayugraIds[i]);
+            }
+
+        }
+
+        private void disconnect()
+        {
+            client.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            refreshList();
+
+            //Get data from NamedPipe
+            /*
+            using (StreamReader sr = new StreamReader(client))
+            {
+                // Display the read text to the console
+                string temp;
+                while ((temp = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine("Received from server: {0}", temp);
+                }
+            }*/
+
+        }
+
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            string id = ayugra_list.SelectedItem.ToString();
+            if (id != null)
+            {
+                client = connection(id);
+            }
+        }
+
+        private void disconnect_button_Click(object sender, EventArgs e)
+        {
+            disconnect();
+        }
+
+        private void refresh_button_Click(object sender, EventArgs e)
+        {
+            refreshList();
+        }
+    }
+}
